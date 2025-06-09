52d084c11f984717a91fc1799b10b8496876b621
IanTuitoek <iantuitoek4@gmail.com>
2025-06-09 15:45:49 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Follow along with the examples here
+function doNothing(){}
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello()
+
+function sayHelloToGuadalupe(){
+    console.log("Hello, Guadalupe")
+}
+
+sayHelloToGuadalupe()
+
+function doSomething(thing) {
+  console.log(thing);
+}
+
+doSomething("anything"); // passing the argument 'anything' into our function
+
+function sayHelloTo(firstname){
+    console.log(`Hello, ${firstname}`)
+}
+
+sayHelloTo("John")
+sayHelloTo(1)
+
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`);
+}
+
+say("Hello", "Judah")
+
+function add(x, y) {
+  return x + y;
+}
+console.log(add(1,2))
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+  return `${greeting}, ${firstName}!`;
+}
+console.log(say("Howdy", "partner"));
